b7474b1983a3620738e75873eee986fa41affe4e
syokora0105 <68280878+syokora0105@users.noreply.github.com>
2020-07-14 21:36:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
-token = os.environ['NzAxMzQ2MTg2ODQ5NjgxNDQ5.Xw2lHg.puQvlQM94MB8TdhsJv2O1Se2tRs']
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
