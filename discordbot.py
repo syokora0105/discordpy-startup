c0593c542e1c3158a8ac8be93d9bdeab6dc8fe45
syokora0105 <68280878+syokora0105@users.noreply.github.com>
2020-08-02 18:23:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ import os
 import traceback
 
 bot = commands. Bot(command_prefix='/')
-token = os.environ[ 'DISCORD_BOT_TOKEN']
+token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_message (message):
